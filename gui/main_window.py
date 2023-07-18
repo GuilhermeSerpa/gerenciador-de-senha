7e02423409bcac62c315d034fdb1c82d0cdb2519
guilherme De Medeiros Serpa <93152552+GuilhermeSerpa@users.noreply.github.com>
2023-07-17 21:53:36 -0300

    Update main_window.py

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,4 +1,3 @@
-# main_window.py
 from PySide6.QtWidgets import QApplication, QMainWindow, QListWidget, QPushButton, QVBoxLayout, QWidget
 
 
@@ -9,15 +8,12 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Gerenciador de Senhas")
         self.resize(400, 300)
 
-        # Lista de senhas
         self.password_list = QListWidget()
-
-        # Botões
+        
         self.add_button = QPushButton("Adicionar Senha")
         self.view_button = QPushButton("Visualizar Senha")
         self.delete_button = QPushButton("Excluir Senha")
 
-        # Layout principal
         layout = QVBoxLayout()
         layout.addWidget(self.password_list)
         layout.addWidget(self.add_button)
